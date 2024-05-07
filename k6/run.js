b24389743e40e162d689fc374a8e70692fb068de
Sourabh Sharma <sourabh.sharma@siya.tech>
2024-05-07 19:08:56 +0530

    integrated raw queries in dimension properties api

--- k6/run.js
+++ k6/run.js
@@ -4,18 +4,18 @@ import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporte
 
 export const options = {
     vus: 50,
-    iterations: 100,
+    // iterations: 100,
     duration: '1m'
 };
 
-let endpoint = '/data_source/10497/action/10503/mappings?version_id=10687&map_type=dimension&scenario_id=13146';
+let endpoint = '/dimension/31428/items/properties?scenario_id=13146';
 
 export default function () {
     const url = `http://127.0.0.1:5000/${endpoint}`;
 
     const headers = {
-        'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMDE1NiwiZXhwIjoxNzEzNzgxMjMwfQ.zVwCp49Rkj6lHCWRpqn0aIxsRZN2zlWHBN-n4ttnaAQ',
-        'Cookie': 'session=.eJxFT1tvgjAY_S99FtIPSgHfHDMTNrpAMiS-GKp1Fli9UAN22X9f3cteTnJyLjnnG13F5SYGvZV7NEd7aA5xTKgTAucOAYqdGFvgHPuU0gjTJkKz_4zaavkl0BxC8H0_iCihQeSGHg5mSJ86oWynuGdY1Av5LrPXCgr5lmRH_rJ78PTDpMBkFrvWBDuvutde36Xtacqfl1YY5aY-jg_OzNIw8znlbT4eCtdUY3Imcdm1tF8l6_J8UbhJp6HcMM_069UTcxTRWjWLws69DeL69w8wBPTnF21VTME.Zh9uXQ.bLaBHxlFjAx6m7zzFWu_1bNk-AY'
+        'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMDE1NiwiZXhwIjoxNzE1NjQ2NjI3fQ.Pui3-3J30iHy-ff6G1-z-lUkd7yTRl6nPHeejRgJ0hU',
+        // 'Cookie': 'session=.eJxFT1tvgjAY_S99FtIPSgHfHDMTNrpAMiS-GKp1Fli9UAN22X9f3cteTnJyLjnnG13F5SYGvZV7NEd7aA5xTKgTAucOAYqdGFvgHPuU0gjTJkKz_4zaavkl0BxC8H0_iCihQeSGHg5mSJ86oWynuGdY1Av5LrPXCgr5lmRH_rJ78PTDpMBkFrvWBDuvutde36Xtacqfl1YY5aY-jg_OzNIw8znlbT4eCtdUY3Imcdm1tF8l6_J8UbhJp6HcMM_069UTcxTRWjWLws69DeL69w8wBPTnF21VTME.Zh9uXQ.bLaBHxlFjAx6m7zzFWu_1bNk-AY'
     }
 
     const response = http.get(url, { headers });
